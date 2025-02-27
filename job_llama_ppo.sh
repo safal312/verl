c6c115fd2675ff285901700482a0d3fcc3613ab8
minwukim <minwu1221@gmail.com>
2025-02-27 18:24:01 +0400

    update ppo llama file

---
+++ job_llama_ppo.sh
@@ -0,0 +1,46 @@
+
+python3 examples/data_preprocess/math_dataset_refactored.py --local_dir ~/data/math 
+
+python3 -c "import transformers; transformers.pipeline('text-generation', model='meta-llama/Llama-3.1-8B')" 
+
+export VLLM_ATTENTION_BACKEND=XFORMERS 
+
+set -x 
+
+python3 -m verl.trainer.main_ppo \
+    data.train_files=$HOME/data/math/train.parquet \
+    data.val_files=$HOME/data/math/test.parquet \
+    data.train_batch_size=128 \
+    data.max_prompt_length=4000 \
+    data.max_response_length=8000 \
+    actor_rollout_ref.model.path=meta-llama/Llama-3.1-8B \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.model.use_remove_padding=True \
+    actor_rollout_ref.actor.ppo_mini_batch_size=256 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=16 \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=4 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.4 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=32 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    critic.optim.lr=1e-5 \
+    critic.model.use_remove_padding=True \
+    critic.model.path=meta-llama/Llama-3.1-8B \
+    critic.model.enable_gradient_checkpointing=True \
+    critic.ppo_micro_batch_size_per_gpu=32 \
+    critic.model.fsdp_config.param_offload=False \
+    critic.model.fsdp_config.optimizer_offload=False \
+    algorithm.kl_ctrl.kl_coef=0.001 \
+    trainer.critic_warmup=0 \
+    trainer.logger=['console','wandb'] \
+    trainer.project_name='project_ppo_llama' \
+    trainer.experiment_name='experiment_ppo_llama' \
+    trainer.n_gpus_per_node=8 \
+    trainer.nnodes=1 \
+    trainer.save_freq=-1 \
+    trainer.test_freq=1 \
+    trainer.total_epochs=15 $@
