f9c08afa23508718c35984e89fb94aff3e0341ca
minwu <mwk300@nyu.edu>
2025-02-28 11:36:21 +0000

    hello

--- job_llama_ppo.sh
+++ job_llama_ppo.sh
@@ -1,7 +1,7 @@
 
 python3 examples/data_preprocess/math_dataset_refactored.py --local_dir ~/data/math 
 
-python3 -c "import transformers; transformers.pipeline('text-generation', model='meta-llama/Llama-3.1-8B')" 
+#python3 -c "import transformers; transformers.pipeline('text-generation', model='Qwen/Qwen2.5-7B-Instruct')" 
 
 export VLLM_ATTENTION_BACKEND=XFORMERS 
 
@@ -10,10 +10,10 @@ set -x
 python3 -m verl.trainer.main_ppo \
     data.train_files=$HOME/data/math/train.parquet \
     data.val_files=$HOME/data/math/test.parquet \
-    data.train_batch_size=128 \
+    data.train_batch_size=256 \
     data.max_prompt_length=4000 \
     data.max_response_length=8000 \
-    actor_rollout_ref.model.path=meta-llama/Llama-3.1-8B \
+    actor_rollout_ref.model.path=Qwen/Qwen2.5-7B-Instruct \
     actor_rollout_ref.actor.optim.lr=1e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=256 \
@@ -29,7 +29,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     critic.optim.lr=1e-5 \
     critic.model.use_remove_padding=True \
-    critic.model.path=meta-llama/Llama-3.1-8B \
+    critic.model.path=Qwen/Qwen2.5-7B-Instruct \
     critic.model.enable_gradient_checkpointing=True \
     critic.ppo_micro_batch_size_per_gpu=32 \
     critic.model.fsdp_config.param_offload=False \
@@ -37,9 +37,9 @@ python3 -m verl.trainer.main_ppo \
     algorithm.kl_ctrl.kl_coef=0.001 \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
-    trainer.project_name='project_ppo_llama' \
-    trainer.experiment_name='experiment_ppo_llama' \
-    trainer.n_gpus_per_node=8 \
+    trainer.project_name='project_ppo_qwen2.5-7b' \
+    trainer.experiment_name='experiment_ppo_qwen2.5-7b-it' \
+    trainer.n_gpus_per_node=1 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
     trainer.test_freq=1 \
