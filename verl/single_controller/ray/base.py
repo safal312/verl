6a00f77d4f59ee97587fb9e971eaa1fb373171ac
minwukim <minwu1221@gmail.com>
2025-02-28 20:29:33 +0400

    update

--- verl/single_controller/ray/base.py
+++ verl/single_controller/ray/base.py
@@ -273,7 +273,9 @@ class RayWorkerGroup(WorkerGroup):
                         else:
                             register_center_actor = ray.get_actor(f"{self.name_prefix}_register_center")
                             break
+                    print("here")
                     assert register_center_actor is not None, f"failed to get register_center_actor: {self.name_prefix}_register_center in {list_named_actors(all_namespaces=True)}"
+                    print("here2")
                     rank_zero_info = ray.get(register_center_actor.get_rank_zero_info.remote())
                     self._master_addr, self._master_port = rank_zero_info['MASTER_ADDR'], rank_zero_info['MASTER_PORT']
                     # print(f"rank_zero_info: {rank_zero_info}")
